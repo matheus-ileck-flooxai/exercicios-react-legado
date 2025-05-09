cc399c04b380db6fe4c08a27cc0e51bd1a03bce0
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-09 12:41:28 -0300

    feat: add PrimeiroComponente component example

--- fundamentos-react-legado/exercicios-react/src/index.js
+++ fundamentos-react-legado/exercicios-react/src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
 
 const elemento = document.getElementById('root')
-ReactDOM.render(<h1>Olá React</h1>, elemento)
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente />
+    </div>, elemento)
