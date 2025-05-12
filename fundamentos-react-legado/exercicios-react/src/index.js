7881a5842e5202161608100cdcdb6891f5d3efca
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-12 09:08:07 -0300

    feat: add multiple component

--- fundamentos-react-legado/exercicios-react/src/index.js
+++ fundamentos-react-legado/exercicios-react/src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import PrimeiroComponente from './componentes/PrimeiroComponente'
+import { CompA, CompB } from './componentes/DoisComponentes'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
         <PrimeiroComponente valor="Bom dia" />
+        <CompA valor="Matheus"/>
+        <CompB valor="Daniela"/>
     </div>, elemento)
