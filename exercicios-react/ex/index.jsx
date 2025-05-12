ba415aea7b0efc55ab96c7c4297789cc5636ac8d
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-12 12:41:13 -0300

    feat: add Counter component using redux

--- exercicios-react/ex/index.jsx
+++ exercicios-react/ex/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom"
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import fieldReducer from "./fieldReducer";
-import Field from "./field";
+import Counter from "./Counter";
+import counterReducer from "./counterReducer";
 
 const reducers = combineReducers({
-    field: fieldReducer
+    counter : counterReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste' />
+        <Counter  />
     </Provider>
     ,
     document.getElementById('app'))
